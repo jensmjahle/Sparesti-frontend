a4d2c0f6012399c91fde5668fa44ae4541f6a4b8
Tini Tran <tinit@stud.ntnu.no>
2024-04-23 10:42:25 +0200

    created a economyview

--- src/views/HomePage/EconomyView.vue
+++ src/views/HomePage/EconomyView.vue
@@ -1,71 +1,164 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
+import {Pie} from 'vue-chartjs'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+ChartJS.register(ArcElement,Tooltip,Legend)
+import TransactionComponent from '@/components/economy/TransactionComponent.vue'
+import ToggleButton from '@/components/economy/ToggleButton.vue'
 
-const selectedOption = ref<string | null>(null)
-const dropdownOptions = [
-  'Option 1',
-  'Option 2',
-  'Option 3'
-]
+const selectedOption = ref<string | null>("")
 
-const handleSelectionChange = (value: string) => {
+
+let page = 0;
+let pages = 0;
+let currentPage = 0;
+
+const transactions = ref([
+  { id: 1,
+    title: 'Rema 1000',
+    date: '2022-05-10',
+    amount: 100,
+    category: 'Dagligvare'
+  },
+  { id: 2,
+    title: 'Trondheim Kino',
+    date: '2022-05-15',
+    amount: 500,
+    category: 'Underholdning'
+  },
+  { id: 3,
+    title: 'SIT',
+    date: '2022-05-15',
+    amount: 4450,
+    category: 'regninger'
+  },
+  { id: 4,
+    title: 'Superhero Burger',
+    date: '2022-05-15',
+    amount: 1500,
+    category: 'Mat & Restaurant'
+  },
+  { id: 6,
+    title: 'Kiwi',
+    date: '2022-05-20',
+    amount: 100,
+    category: 'Dagligvare'
+  },
+])
+const chartVisible = ref(false)
+
+const toggleChart = (value) => {
+  chartVisible.value = value
+  console.log(chartVisible.value)
+}
+const handleSelectionChange = (value: string | null) => {
   selectedOption.value = value
+}
+
+const distinctCategories = computed(() => {
+  const categories = new Set<string>()
+  transactions.value.forEach(transaction => {
+    categories.add(transaction.category)
+  })
+  return Array.from(categories)
+})
+
+const dropdownOptions = computed(() => {
+  return ['Alle', ...distinctCategories.value]
+})
+
+const filteredTransactions = computed(() => {
+  if (selectedOption.value === 'Alle' || !selectedOption.value) {
+    return transactions.value
+  } else {
+    return transactions.value.filter(transaction => transaction.category === selectedOption.value)
+  }
+})
+const chartData = computed(() => {
+  const data: { labels: string[], datasets: { data: number[], label:string ,backgroundColor: string[] }[] } = {
+    labels: [],
+    datasets: [{
+      label: "kr",
+      data: [],
+      backgroundColor: [],
+    }]
+  }
+
+  const categoryAmounts: { [key: string]: number } = {};
+
+  transactions.value.forEach(transaction => {
+    const { category, amount } = transaction;
+    if (category in categoryAmounts) {
+      categoryAmounts[category] += amount;
+    } else {
+      categoryAmounts[category] = amount;
+      data.labels.push(category);
+      console.log(data.datasets)
+      data.datasets[0].backgroundColor.push(getRandomColor());
+    }
+  });
+
+  data.labels.forEach(label => {
+    data.datasets[0].data.push(categoryAmounts[label]);
+  });
+  console.log(data)
+  return data;
+})
+
+// Function to generate random color
+const getRandomColor = () => {
+  const letters = '0123456789ABCDEF'
+  let color = '#'
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)]
+  }
+  return color
 }
 </script>
 
 <template>
   <div class="economy-ui">
 
-  <h2>Dine forbruk</h2>
+  <h2>Ditt forbruk</h2>
+
   <div class="container">
-    <div class="box">
+    <div class="toggle-button-container">
+      <ToggleButton @toggle-chart="toggleChart" />
+      <span> Vis graf </span>
+    </div>
+    <div class="box" :class="{ 'mobile-hide': chartVisible }">
       <div class="custom-dropdown-container">
-        <select class="custom-dropdown" v-model="selectedOption" @change="handleSelectionChange($event.target.value)">
-          <option disabled value="">Filtrer dine transaksjoner</option>
+        <select class="custom-dropdown" v-model="selectedOption" @change="handleSelectionChange(selectedOption)">
+          <option disabled value="" selected>Velg kategori</option>
           <option v-for="option in dropdownOptions" :key="option" :value="option">{{ option }}</option>
         </select>
       </div>
       <div class="component-container">
-        <div class="component">
-          <div class="component-left">
-            <div class="component-title">
-              Rema 1000
-            </div>
-          </div>
-          <div class="component-right">
-            <div class="component-right-field">
-              <span> Date: 10.05.2012</span>
-            </div>
-            <div class="component-right-field">
-              <span>
-                100 kr
-              </span>
-            </div>
-            <div class="component-right-field">
-              <span>
-                Kategori: Mat
-              </span>
-            </div>
-
-
-          </div>
-        </div>
-        <div class="component">
-
-        </div>
-        <div class="component">
-
-        </div>
-        <div class="component">
-
-        </div>
-        <div class="component">
-
-        </div>
+        <transaction-component
+        v-for="transaction in filteredTransactions"
+        :key="transaction.id"
+        :title="transaction.title"
+        :category="transaction.category"
+        :amount="transaction.amount"
+        :date="transaction.date"
+        ></transaction-component>
       </div>
-
+      <div class="pagination">
+        <button :disabled="currentPage === 0">Previous</button>
+        <div  v-if="pages>0" class="page-numbers">
+          <button
+            v-for="pageNumber in pages"
+            :key="pageNumber-2"
+            :class="{ active: pageNumber-1 === currentPage }"
+          >{{ pageNumber}}</button>
+        </div>
+        <button :disabled="currentPage === pages - 1 || pages === 0">Next</button>
+      </div>
     </div>
-  </div>
+      <div id="pie-box" :class="{ 'mobile-show': chartVisible }">
+          <Pie :data="chartData" :options="{ maintainAspectRatio: false }"></Pie>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -93,108 +186,150 @@ h2 {
 .custom-dropdown-container {
   display: flex;
   flex-direction: column;
+  justify-content: center;
   align-items: center;
 }
 
 .container {
   display: flex;
-  height: 100vh;
-}
-.component-left{
-  width:50%;
-  height:100%;
-}
-.component-right {
-  display: flex;
-  flex-direction: column;
-  width: 50%;
   height: 100%;
 }
+
 .component-container {
-  height: 100vh;
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-top: 1.25%;
 }
-
+#pie-box,
 .box {
   border-radius: 20px;
-  border: 1px solid #000;
-  width:80%;
+  border: 1px solid var(--color-border);
+  width:100%;
   max-width: 600px; /* Limit maximum width */
-  min-height: 250px;
-  height: 75%;
-  box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+  min-height: 510px;
+  height: auto;
+  box-shadow: 0 4px 4px 0 var(--color-shadow);
+  transition: transform 0.3s ease-in-out;
+  background-color: var(--color-background);
+  margin-right: 1%;
+
 }
 
-.component {
-  border-radius: 20px;
-  border: 1px solid #000;
-  /*box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25); */
-  width: 80%;
-  height: 10%;
-  margin-left: 5%;
-  margin-bottom: 2.5%;
-  display:flex;
-}
+
 
 .custom-dropdown {
   width: 80%;
   height: 7%;
   padding: 8px;
   margin-top: 2%;
-  margin-bottom: 5%;
+  margin-bottom: 2%;
   border-radius: 20px;
 }
 
-.component-title {
-  height: 100%;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  padding-left: 20px;
-  font-size:1.5em
 
+
+.pagination {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
-.component-right-field {
+.pagination button {
+  padding: 5px 10px;
+  margin: 0 5px;
+  border: 1px var(--color-border);
+  border-radius: 3px;
+  background-color: transparent;
+  cursor: pointer;
+}
+
+.pagination button:hover {
+  background-color: #f0f0f0;
+}
+
+.pagination button:disabled {
+  color: #ccc;
+  cursor: not-allowed;
+}
+
+.page-numbers {
   display: flex;
-  height: 100%;
-  width: 95%;
-  justify-content: right;
-  font-size: 1em;
+}
+
+.page-numbers button {
+  padding: 5px 10px;
+  margin: 0 2px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+  background-color: transparent;
+  cursor: pointer;
+}
+
+.page-numbers button.active {
+  background-color: #5E5CE5;
+  color: white;
+}
+
+.page-numbers button:hover {
+  background-color: #f0f0f0;
+}
+
+.page-numbers button:disabled {
+  color: #ccc;
+  cursor: not-allowed;
+}
+
+.toggle-button-container{
+  display: none;
 }
 
 
 @media screen and (max-width: 600px) {
   .box {
     width: 100%;
-    margin: 10px; /* Adjust margin for smaller screens */
+    min-height: 510px;
+    margin:10px /* Adjust margin for smaller screens */
   }
 
   .container {
-    flex-direction: row; /* Change flex direction for smaller screens */
+    flex-direction: column; /* Change flex direction for smaller screens */
+    align-content: center;
+    height: 100%;
   }
 
   h2 {
-    font-size: 1.5em; /* Adjust font size for smaller screens */
+    font-size: 3em; /* Adjust font size for smaller screens */
+  }
+  span {
+    font-size: 1.9em;
+  }
+  .toggle-button-container {
+    display:block;
+    margin-left: 6%;
+    justify-content: center;
   }
 
-  .component-title {
-    font-size: 1.2em; /* Adjust font size for smaller screens */
-    height: 100%;
-    width: 100%;
-    display: flex;
-    align-items: center;
-    padding-left: 20px;
+  .custom-dropdown{
+    font-size: 1.8em;
   }
 
-  .component-right-field {
-    display: flex;
-    height: 100%;
-    width: 95%;
-    justify-content: right;
-    font-size: 0.8em; /* Adjust font size for smaller screens */
+
+  #pie-box {
+    display: none; /* Show the graph container on mobile by default */
+    height: 650px;
   }
+
+  /* Show the graph container only when chartVisible is true */
+  .mobile-show {
+    display: block !important;
+  }
+
+  /* Hide the transactions box when chartVisible is true */
+  .box.mobile-hide {
+    display: none;
+  }
+
 }
-
 </style>
