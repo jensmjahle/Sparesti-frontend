45d3a1a52c940b91f09709c15ac8df7e89a444f7
BiggestGuz <Diblopro@gmail.com>
2024-04-24 14:42:01 +0200

    Fix utils and store

--- src/stores/token.ts
+++ src/stores/token.ts
@@ -1,16 +1,17 @@
 import { defineStore } from "pinia";
 import {
     getJwtToken,
-    getUserInfo
+    getUserInfo,
+    refreshToken
 } from "@/utils/frontPageUtils";
+import router from "@/router";
 
 export const useTokenStore = defineStore('token', {
     state: () => ({
-        timer: null,
+        timer: null as number | null,
         jwtToken: "",
         username: null,
         isConnectedToBank: null
-
     }),
 
     persist: {
@@ -37,10 +38,41 @@ export const useTokenStore = defineStore('token', {
                         })
                     }
                 }
+                this.startTimer();
             } catch (error) {
                 console.error(error);
             }
-        }
+        },
+
+        async refreshToken() {
+            try {
+                const response = await refreshToken(this.jwtToken);
+                if (response !== undefined) {
+                    this.jwtToken = response.data;
+                    this.startTimer();
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        },
+
+        logout() {
+            this.jwtToken = "";
+            this.username = null;
+            this.isConnectedToBank = null;
+            router.push("/login").then(r => r);
+        },
+
+        startTimer() {
+            this.timer = setTimeout(() => {
+                // Display a dialog box to the user
+                if (window.confirm("Your session is about to expire. Do you want to extend it?")) {
+                    this.refreshToken().then(r => r);
+                } else {
+                    this.logout();
+                }
+            }, 10000);
+        },
     },
 
     getters: {
