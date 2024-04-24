bd1d3f3011cbd8ddcb4139f8dc160e805e727e50
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2024-04-24 09:56:33 +0200

    Add empty Milestone displays

---
+++ src/components/milestone/ActiveMilestoneDisplay.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const props = defineProps({
+  id: Number,
+  title: String,
+  description: String
+});
+</script>
+
+<template>
+<div class="active-milestone-display">
+  <h3 class="title">{{props.title}}</h3>
+  <h4 class="description">{{props.description}}</h4>
+  <div class="button-container">
+    <button class="complete-button">
+      <h3 class="complete-button-text">Fullfør</h3>
+    </button>
+  </div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/milestone/CompletedMilestoneDisplay.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<h3>gdfg</h3>
+</template>
+
+<style scoped>
+
+</style>
