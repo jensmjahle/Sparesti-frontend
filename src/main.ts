3200d97b6c69e66635aff8bcc285a3a21e3aea95
Tini Tran <tinit@stud.ntnu.no>
2024-04-24 11:10:48 +0200

    Added Datepicker dependency for vue

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +12,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.component('VueDatePicker', VueDatePicker);
 app.mount('#app')
