2aa9703495ac448f79bf0654b7fc74ed203aabfc
Sverre <sverrefh@stud.ntnu.no>
2024-04-29 13:20:40 +0200

    Added unit test to InactiveChallengeDisplay

--- src/components/__tests__/ActiveChallengeDisplay.spec.ts
+++ src/components/__tests__/ActiveChallengeDisplay.spec.ts
@@ -2,7 +2,7 @@ import { mount } from '@vue/test-utils';
 import ActiveChallengeDisplay from '@/components/challenge/ActiveChallengeDisplay.vue';
 import { describe, it, expect } from 'vitest'
 
-describe('MyComponent', () => {
+describe('ActiveChallengeDisplay', () => {
   it('emits challengeCompleted when complete button is clicked', async () => {
     const challenge = {
       challengeId: 1,
@@ -21,7 +21,7 @@ describe('MyComponent', () => {
     await wrapper.find('.complete-button').trigger('click');
 
     expect(wrapper.emitted('challengeCompleted')).toBeTruthy();
-    expect(wrapper.emitted('challengeCompleted')[0][0]).toBe(challenge.challengeId);
+    expect(wrapper.emitted('challengeCompleted')?.[0]?.[0]).toBe(challenge.challengeId);
   });
 
   it('emits challengeDeleted when delete button is clicked', async () => {
@@ -42,7 +42,7 @@ describe('MyComponent', () => {
     await wrapper.find('.close-img').trigger('click');
 
     expect(wrapper.emitted('challengeDeleted')).toBeTruthy();
-    expect(wrapper.emitted('challengeDeleted')[0][0]).toBe(challenge.challengeId);
+    expect(wrapper.emitted('challengeDeleted')?.[0]?.[0]).toBe(challenge.challengeId);
   });
 
   it('displays correct challenge information', () => {
