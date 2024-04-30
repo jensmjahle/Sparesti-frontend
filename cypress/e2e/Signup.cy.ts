29a64989b6f3af9f45d07312b6f32def6a1886a8
Sverre <sverrefh@stud.ntnu.no>
2024-04-30 11:09:00 +0200

    Added e2e test to Signup view

---
+++ cypress/e2e/Signup.cy.ts
@@ -0,0 +1,26 @@
+describe('Signup spec', () => {
+  it('Can signup', () => {
+    cy.visit('/signup')
+    cy.get('[data-testid="SignupButton"]').should('be.disabled')
+
+    cy.get('[data-testid="NameInput"]').type("TestingUser")
+    cy.get('[data-testid="NameInput"]').should('have.value', "TestingUser")
+
+    cy.get('[data-testid="SignupButton"]').should('be.disabled')
+
+    cy.get('[data-testid="EmailInput"]').type("Testing@Test.test")
+    cy.get('[data-testid="EmailInput"]').should('have.value', "Testing@Test.test")
+
+    cy.get('[data-testid="SignupButton"]').should('be.disabled')
+
+    cy.get('[data-testid="PasswordInput"]').type("ehswbfwowqw+pdiaas1")
+    cy.get('[data-testid="PasswordInput"]').should('have.value', "ehswbfwowqw+pdiaas1")
+
+    cy.get('[data-testid="SignupButton"]').should('be.disabled')
+
+    cy.get('[data-testid="ConfirmPasswordInput"]').type("ehswbfwowqw+pdiaas1")
+    cy.get('[data-testid="ConfirmPasswordInput"]').should('have.value', "ehswbfwowqw+pdiaas1")
+
+    cy.get('[data-testid="SignupButton"]').should('be.enabled')
+  })
+})
