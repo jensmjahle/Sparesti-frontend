0df94e8e9f024964286c983256c91f849aa99e4f
Sverre <sverrefh@stud.ntnu.no>
2024-04-30 08:58:35 +0200

    Wrote rendering unit test for IncomeInfo component

---
+++ src/components/__tests__/IncomeInfo.spec.ts
@@ -0,0 +1,21 @@
+import { mount } from '@vue/test-utils';
+import IncomeInfo from '@/components/profile/IncomeInfo.vue'
+import { describe, it, expect, beforeEach } from 'vitest'
+import { createPinia, setActivePinia } from 'pinia'
+
+describe('IncomeInfo', () => {
+
+  beforeEach(() => {
+    setActivePinia(createPinia());
+  });
+
+  it('renders correctly', async () => {
+    const wrapper = mount(IncomeInfo);
+
+    expect(wrapper.exists()).toBe(true);
+
+    expect(wrapper.findAll('.input-field')).toHaveLength(3);
+
+    expect(wrapper.find('.save-button').exists()).toBe(true);
+  });
+});
