81aa7015bf63503a9fe1c026dced205b37e2c6f3
Sverre <sverrefh@stud.ntnu.no>
2024-05-02 14:16:54 +0200

    Updated text contents of splash page

--- src/components/InfoComponent.vue
+++ src/components/InfoComponent.vue
@@ -13,7 +13,9 @@ onMounted(async () => {
 
 <template>
   <div id = InfoComponent>
-    <h2 id = Quote><strong>Sparetips: </strong>Sett av et jevnt beløp hver måned på sparekonto eller lignende.</h2>
+    <h2 id = Quote>Velkommen til Sparesti! Sparesti er en applikasjon for å hjelp deg med å spare penger
+    mot dine drømmer ved hjelp av en rekke spesialtilpassede utfordinger basert på ditt forbruk! Kan du hjelpe
+    sparegrisen ned stien mot de store målene?</h2>
     <h2 id = TotalSavings>Våre brukere har spart til sammen <strong>{{ totalSavings }}</strong> nok</h2>
   </div>
 </template>
@@ -29,10 +31,11 @@ onMounted(async () => {
     border: 2px solid var(--color-border);
     box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.6);
     height: 60vh;
+    width: 100%;
   }
 
   #Quote, #TotalSavings{
-    padding: 5%;
+    padding: 3%;
   }
 
   #TotalSavings strong , #Quote strong{
