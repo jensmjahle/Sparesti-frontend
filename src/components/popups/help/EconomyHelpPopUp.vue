b1053028041216c1ec4c53c8ef55465621658a6e
Sverre <sverrefh@stud.ntnu.no>
2024-05-02 15:09:32 +0200

    Updated info PopUps

--- src/components/popups/help/EconomyHelpPopUp.vue
+++ src/components/popups/help/EconomyHelpPopUp.vue
@@ -15,7 +15,7 @@ const closePopUp = () => {
     </div>
     <h3>
       Hei! Du befinner deg for øyeblikket på transaksjon-siden til sparesti.
-      På denne siden finner du en oversikt over dine nyligste transaksjoner.
+      På denne siden finner du en oversikt over dine nyligste transaksjoner, samt hva pengene har gått til
     </h3>
 
     <div class="option-buttons">
